f84541d9bad2a889bc7f800cba4b358c7ffcd2fb
Nathi <nathalia.cappellini1@gmail.com>
2024-07-17 16:29:26 -0300

    Commit Inicial

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,19 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TO DO: Conhecer e importar a classe scanner
+
+        //Exibir as mensagens para o nosso usuario
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+
+        int numero = 1500;
+        String agencia = "059-5";
+        String nomeCliente = "AMANDA PEREIRA";
+        double saldo = 900.00;
+
+
+
+    }
+}
